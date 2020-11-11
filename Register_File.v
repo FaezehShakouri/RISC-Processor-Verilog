3a154778a4eac984d670a53b67c0df4ea90c8381
FaezeShakuri <f.shakori.com@gmail.com>
2020-11-11 23:41:15 +0330

    register_file added

---
+++ Register_File.v
@@ -0,0 +1,56 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:33:31 11/11/2020 
+// Design Name: 
+// Module Name:    Register_File 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Register_File(clk ,RegWrite, read_reg_1, read_reg_2, write_reg, write_data, read_data_1, read_data_2);
+	input clk, RegWrite;
+	
+	// Write port
+	input [2:0] write_reg;
+	input [15:0] write_data;
+	
+	// Read ports
+	input [2:0] read_reg_1;
+	input [2:0] read_reg_2;
+	output [15:0] read_data_1;
+	output [15:0] read_data_2;
+	
+	reg [15:0] registers [7:0];
+	
+	initial begin
+		registers[0] = 0;
+		$readmemh("register_file.txt", registers);
+	end
+	
+	
+	// Reading doesn't have clk
+	assign read_data_1[15:0] = registers[read_reg_1];
+	assign read_data_2[15:0] = registers[read_reg_2];
+		
+		
+	// Write in register file must be done at posedge clk
+	always @ (posedge clk)
+	begin
+		if (RegWrite && (write_reg != 0))	// can't write in registers[0]
+		begin
+			registers[write_reg] = write_data;
+		end
+	end
+
+endmodule
---
+++ register_file_test.v
@@ -0,0 +1,73 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   23:12:11 11/11/2020
+// Design Name:   Register_File
+// Module Name:   D:/Uni/Term 7/Architecture Lab/CPU/RISC_Processor/register_file_test.v
+// Project Name:  RISC_Processor
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: Register_File
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module register_file_test;
+
+	// Inputs
+	reg clk;
+	reg RegWrite;
+	reg [2:0] read_reg_1;
+	reg [2:0] read_reg_2;
+	reg [2:0] write_reg;
+	reg [15:0] write_data;
+
+	// Outputs
+	wire [15:0] read_data_1;
+	wire [15:0] read_data_2;
+
+	// Instantiate the Unit Under Test (UUT)
+	Register_File uut (
+		.clk(clk), 
+		.RegWrite(RegWrite), 
+		.read_reg_1(read_reg_1), 
+		.read_reg_2(read_reg_2), 
+		.write_reg(write_reg), 
+		.write_data(write_data), 
+		.read_data_1(read_data_1), 
+		.read_data_2(read_data_2)
+	);
+	
+	initial begin
+		forever begin
+			$display(read_data_1);
+			#20;
+			clk = ~clk;
+			
+		end
+	end
+	
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		RegWrite = 1;
+		read_reg_1 = 4;
+		read_reg_2 = 0;
+		write_reg = 4;
+		write_data = 20;
+	
+
+	end
+      
+endmodule
+
